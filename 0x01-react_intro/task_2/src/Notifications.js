6bb405a7f120c66adc2260435ac785af9af37c8d
Demilade362 <ademolademilade362@gmail.com>
2024-04-03 16:52:48 -0700

    modify notifications

--- 0x01-react_intro/task_2/src/Notifications.js
+++ 0x01-react_intro/task_2/src/Notifications.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './Notifications.css';
+import closeIcon from './close-icon.png';
 
 function Notifications() {
+    // Function to handle button click event
+    const handleButtonClick = () => {
+        console.log('Close button has been clicked');
+    };
+
     return (
         <div className="Notifications">
+            <button
+                style={{ float: 'right' }}
+                aria-label="Close"
+                onClick={handleButtonClick}
+            >
+                <img src={closeIcon} alt="Close" />
+            </button>
             <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
         </div>
     );
 }
