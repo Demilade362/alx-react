8558e9dbf4c0030375213c9882dc8475dc6c7daf
Demilade362 <ademolademilade362@gmail.com>
2024-04-03 18:24:57 -0700

    done

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,33 +1,24 @@
-import React from 'react';
+import logo from './holberton-logo.jpg';
 import './App.css';
 import { getFullYear, getFooterCopy } from './utils';
-import holbertonLogo from './Holberton_Logo.png';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={holbertonLogo} className="App-logo" alt="logo" />
-                <h1>School dashboard</h1>
-            </header>
-            <div className="App-body">
-                <p>Login to access the full dashboard</p>
-                <div className="form-group">
-                    <label htmlFor="email" onClick={() => handleLabelClick('email')}>Email:</label>
-                    <input type="email" id="email" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password" onClick={() => handleLabelClick('password')}>Password:</label>
-                    <input type="password" id="password" />
-                </div>
-                <button type="button">OK</button>
-            </div>
-            <footer className="App-footer">
-                <p>{getFooterCopy(true)}</p>
-                <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
-            </footer>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<div className='App-header'>
+				<img src={logo} alt='Holberton' />
+				<h1>School dashboard</h1>
+			</div>
+			<div className='App-body'>
+				<p>Login to access the full dashboard</p>
+			</div>
+			<div className='App-footer'>
+				<p>
+					Copyright {getFullYear()} - {getFooterCopy(true)}
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -1,7 +1,4 @@
-export function getFullYear() {
-    return new Date().getFullYear();
-}
+export const getFullYear = () => new Date().getFullYear();
 
-export function getFooterCopy(isIndex) {
-    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
+export const getFooterCopy = (isIndex) =>
+	isIndex ? 'Holberton School' : 'Holberton School main dashboard';
